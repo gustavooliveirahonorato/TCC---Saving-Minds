52cc1e6092c6a53a39fae15a9033e8db0e6cf8ae
Yasmin Dias <yasminsilvad08@gmail.com>
2026-09-16 11:36:38 -0300

    Atualiza conexao para TiDB Cloud

--- save-minds/conexao.php
+++ save-minds/conexao.php
@@ -1,13 +1,19 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$senha = ""; 
-$banco = "saving_minds";
+$host = "gateway01.us-east-1.prod.aws.tidbcloud.com";
+$usuario = "Wen9maK69CYDqnX.root";
+$senha = "FCzmAOxZakHLB0aa"; 
+$banco = "sys";
+$porta = 4000;
 
-$conn = new mysqli($host, $usuario, $senha, $banco);
+// Cria a conexão incluindo a porta
+$conn = mysqli_init();
 
-if ($conn->connect_error) {
-    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+// Configuração SSL exigida pelo TiDB Cloud
+// (Se faltar isso, a conexão de fora da rede deles pode ser rejeitada)
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+if (!mysqli_real_connect($conn, $host, $usuario, $senha, $banco, $porta, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
 }
 
 $conn->set_charset("utf8mb4");
